156d01cb4ed7fda5dfe4b00e05b912a873b686c6
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2023-12-29 21:21:25 +0100

    Feature 1: Basic server setup (#1)
    * Go mod changes
    
    * Added test for GET and POST requests
    
    * Created Event struct with methods
    
    * Created basic wer server with post and get endpoints

--- main.go
+++ main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/JarGad23/go-rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not pares request data"})
+		return
+	}
+
+	event.Id = 1
+	event.UserId = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
+
+}
