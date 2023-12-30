4921014445cfd54306789796525635cd36539ebf
JarGad23 <j.gad@m8b.pl>
2023-12-30 21:46:01 +0100

    Handled errors in main go file and added get event by id route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/JarGad23/go-rest-api/db"
 	"github.com/JarGad23/go-rest-api/models"
@@ -13,14 +14,39 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events"})
+		return
+	}
+
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
@@ -35,7 +61,12 @@ func createEvent(context *gin.Context) {
 	event.Id = 1
 	event.UserId = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not create event" + err.Error()})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
 
