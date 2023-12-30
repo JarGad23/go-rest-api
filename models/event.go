0caec8765f5fcab11583d2fc5768300e6ab9694b
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2023-12-30 17:18:45 +0100

    Feature 2: Setting up database (#2)
    * Go mod updates
    
    * Added db file wit api.db output
    
    * Added insert query and initializat db in main

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/JarGad23/go-rest-api/db"
+)
 
 type Event struct {
-	Id          int
+	Id          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,9 +17,30 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// Add event to database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+		INSERT INTO events(name, description, location, dateTime, userId) 
+		VALUES(?, ?, ?, ?, ?)
+	`
+	sqlStmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer sqlStmt.Close()
+
+	result, err := sqlStmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	e.Id = id
+
+	return err
 }
 
 func GetAllEvents() []Event {
