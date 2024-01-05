fcbfb42c7b72c8fb8182c213cfe78129dc0063f0
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-01-05 20:03:39 +0100

    Feature 10: Register functionality (#10)
    * Api test changes
    
    * Add register logic and register table
    
    * Added routes and handlers to register functionality

--- models/event.go
+++ models/event.go
@@ -122,3 +122,35 @@ func (e Event) Delete() error {
 
 	return err
 }
+
+func (e Event) Register(userId int64) error {
+	query := "INSERT INTO registrations(event_id, user_id) VALUES(?, ?)"
+
+	sqlStmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer sqlStmt.Close()
+
+	_, err = sqlStmt.Exec(e.Id, userId)
+
+	return err
+}
+
+func (e Event) CancelRegistration(userId int64) error {
+	query := "DELETE FROM registrations WHERE event_id = ? AND user_id = ?"
+
+	sqlStmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer sqlStmt.Close()
+
+	_, err = sqlStmt.Exec(e.Id, userId)
+
+	return err
+}
